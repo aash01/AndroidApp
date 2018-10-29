49f43c8cd8e7fd637b8a38214b4d4b1332c18e88
Aashrav Shetty <aashrav.shetty01@gmail.com>
2018-10-28 17:37:03 -0700

    Final

---
+++ app/src/main/java/com/example/aashr/facerecognition/GuestPicture.java
@@ -0,0 +1,13 @@
+package com.example.aashr.facerecognition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuestPicture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest);
+    }
+}
